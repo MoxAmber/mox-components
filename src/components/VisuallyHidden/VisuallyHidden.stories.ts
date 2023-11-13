410dec4683530299de2b58806f5b309a6f20938f
MoxAmber <amber@realemail.net>
2023-11-13 00:00:12 +0000

    feat(VisuallyHidden): create VisuallyHidden component

---
+++ src/components/VisuallyHidden/VisuallyHidden.stories.ts
@@ -0,0 +1,24 @@
+import { Meta, StoryObj } from "@storybook/web-components";
+import { html } from "lit";
+import ".";
+import { VisuallyHidden } from ".";
+
+interface CustomArgs extends VisuallyHidden {
+  content?: string;
+}
+
+const meta = {
+  component: "mox-visually-hidden",
+  args: {
+    content: "This text is only visible in the accessibility tree.",
+  },
+  render: (args) => html`
+    <mox-visually-hidden ?focusable=${args.focusable}>
+      <a href="#">${args.content}</a>
+    </mox-visually-hidden>
+  `,
+} satisfies Meta<CustomArgs>;
+export default meta;
+
+export const Primary: StoryObj = {};
+export const Focusable: StoryObj = { args: { focusable: true } };
---
+++ src/components/VisuallyHidden/index.ts
@@ -0,0 +1,54 @@
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map.js";
+
+/**
+ * @tag mox-visually-hidden
+ * @summary Visually hide content, while still rendering it in the accessibility tree
+ *
+ * @slot - Any content intended to be hidden visually
+ *
+ * @link https://github.com/h5bp/html5-boilerplate/blob/master/dist/css/main.css
+ * @link http://snook.ca/archives/html_and_css/hiding-content-for-accessibility
+ * @link https://www.drupal.org/node/897638
+ */
+@customElement("mox-visually-hidden")
+export class VisuallyHidden extends LitElement {
+  /**
+   * @attr focusable - Should contents be focusable?
+   */
+  @property({ type: Boolean })
+  focusable = false;
+
+  static styles = css`
+    ::slotted(*) {
+      border: 0;
+      clip: rect(0, 0, 0, 0);
+      height: 1px;
+      margin: -1px;
+      overflow: hidden;
+      padding: 0;
+      position: absolute;
+      white-space: nowrap;
+      width: 1px;
+    }
+
+    .focusable:active,
+    .focusable:focus {
+      clip: auto;
+      height: auto;
+      margin: 0;
+      overflow: visible;
+      position: static;
+      white-space: normal;
+      width: auto;
+    }
+  `;
+
+  render() {
+    const classes = {
+      focusable: this.focusable,
+    };
+    return html`<slot class=${classMap(classes)}></slot>`;
+  }
+}
