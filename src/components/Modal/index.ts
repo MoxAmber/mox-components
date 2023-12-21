b756bdebd0b32ccd270d540b5fa296a1eed3ac20
MoxAmber <amber@realemail.net>
2023-12-21 20:27:57 +0000

    feat(Modal): improve click-outside, add properties
    Adds a container to fix clicking on dialog padding dismissing the modal
    Expose 'open' and 'returnValue' properties, and fire 'close' event to match dialog

--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -1,26 +1,52 @@
 import { LitElement, css, html } from "lit";
-import { customElement, query } from "lit/decorators.js";
+import { customElement, property, query } from "lit/decorators.js";
 
 /**
+ * An accessible modal dialog component with click-outside behaviour
+ *
  * @tag mox-modal
- * @summary An accessible modal dialog component with click-outside behaviour
  *
  * @slot title - A title to display in the Modal header
  * @slot - Modal body content
  *
- * @part dialog - the containing dialog element
- * @part close - the close button
+ * @part dialog - The base dialog element
+ * @part close - The close button
+ * @part container - The inner container div
  *
+ * @fires {Event} close - Fires when the modal is closed
  */
 @customElement("mox-modal")
 export class Modal extends LitElement {
   /**
+   * Inner dialog element
    * @internal
    */
   @query("dialog")
   protected _dialogEl!: HTMLDialogElement;
 
+  /**
+   * Whether the modal is open and available for interaction
+   */
+  @property({ attribute: false })
+  open = false;
+
+  /**
+   * The return value of the modal, either from Modal.close(returnValue) or
+   * submitting <form method='dialog'>
+   */
+  @property({ attribute: false })
+  returnValue = "";
+
   static styles = css`
+    dialog {
+      padding: 0px;
+    }
+    .container {
+      box-sizing: border-box;
+      width: 100%;
+      height: 100%;
+      padding: 16px;
+    }
     header {
       display: grid;
       grid-template-columns: 1fr min-content;
@@ -36,36 +62,75 @@ export class Modal extends LitElement {
 
   render() {
     return html`
-      <dialog part="dialog" @click=${(e: MouseEvent) => this._clickOutside(e)}>
-        <header>
-          <slot name="title"></slot>
-          <button
-            type="button"
-            part="close"
-            aria-label="Close"
-            autofocus
-            @click=${() => this.close()}
-          >
-            X
-          </button>
-        </header>
-        <slot></slot>
+      <dialog
+        part="dialog"
+        @click=${this._onClickOutside}
+        @close=${this._onClose}
+      >
+        <div class="container" part="container">
+          <header>
+            <slot name="title"></slot>
+            <button
+              type="button"
+              part="close"
+              aria-label="Close"
+              @click=${this.close}
+            >
+              X
+            </button>
+          </header>
+          <slot></slot>
+        </div>
       </dialog>
     `;
   }
 
+  /**
+   * Show the modal. Everything outside the modal is inert
+   */
   showModal() {
     this._dialogEl.showModal();
+    this.open = true;
   }
 
+  /**
+   * Close the modal, optionally setting the returnValue
+   * @param {string} returnValue - The return value to set
+   */
   close(returnValue?: string) {
     this._dialogEl.close(returnValue);
   }
 
-  private _clickOutside(e: MouseEvent) {
-    if (e.target === this._dialogEl) {
+  /**
+   * Click outside handler.
+   * If the event target is the dialog element, the user is clicking
+   * on the backdrop, as content within the dialog container will block
+   * the click event.
+   *
+   * @internal
+   * @param {MouseEvent} e - A click event
+   */
+  private _onClickOutside(e: MouseEvent) {
+    if (e.target === e.currentTarget) {
       this.close();
     }
+  }
+
+  /**
+   * Close handler
+   *
+   * @internal
+   * @param {Event} e - Close event
+   */
+  private async _onClose(e: Event) {
+    this.open = false;
+    this.returnValue = (e.target as HTMLDialogElement).returnValue;
+    await this.updateComplete;
+    this.dispatchEvent(
+      new Event("close", {
+        composed: true,
+      }),
+    );
   }
 }
 
